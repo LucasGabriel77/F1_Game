46c347ad35aaceb54eb5d65beb3252024053feb5
NATHAN GUSTAVO PADILHA REICHERT <nathan_reichert@estudante.sesisenai.org.br>
2024-03-06 08:03:53 -0300

    criado a class do objeto

---
+++ formula1_class.js
@@ -0,0 +1,15 @@
+class Obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.a = a
+    }
+
+    des_obj(){
+        
+    }
+        
+    
+}
